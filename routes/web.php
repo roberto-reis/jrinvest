4f4c1ff9251b313df429089debd7d8e2421bd05d
José Roberto <tekinforroberto@gmail.com>
2022-02-03 06:48:36 -0300

    ajuste nas rotas de Operacao

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,6 @@ Route::get('/', function () {
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::prefix('users')->group(function () {
-    Route::get('/', [UserController::class, 'index'])->name('users.index');
-    Route::get('/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/', [UserController::class, 'store'])->name('users.store');
-    Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::get('/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-});
-
 Route::prefix('operacoes')->group(function(){
     Route::get('/', [OperacaoController::class, 'index'])->name('operacoes.index');
     Route::post('/store', [OperacaoController::class, 'store'])->name('operacoes.store');
