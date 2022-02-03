9663046c89a49f6a5c7911f60f5e39f6fb2ab552
José Roberto <tekinforroberto@gmail.com>
2022-02-03 06:42:53 -0300

    feature: criado controller RebalanceamentoClasse

---
+++ app/Domain/RebalanceamentoClasse/Controllers/RebalanceamentoClasseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Domain\RebalanceamentoClasse\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RebalanceamentoClasseController extends Controller
+{
+    //
+}
