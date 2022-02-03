1e6c8b566043477be777210f076dd9eb78412313
José Roberto <tekinforroberto@gmail.com>
2022-02-03 06:40:29 -0300

    feature: criado controller ClasseAtivo

---
+++ app/Domain/ClasseAtivo/Controllers/ClasseAtivoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Domain\ClasseAtivo\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ClasseAtivoController extends Controller
+{
+    //
+}
