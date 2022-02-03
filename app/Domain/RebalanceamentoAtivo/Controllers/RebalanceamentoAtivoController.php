ed7e2ddfe3e0462ee8b96baf78f8f59d9f3a22f3
José Roberto <tekinforroberto@gmail.com>
2022-02-03 06:42:28 -0300

    feature: criado controller RebalanceamentoAtivo

---
+++ app/Domain/RebalanceamentoAtivo/Controllers/RebalanceamentoAtivoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Domain\RebalanceamentoAtivo\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RebalanceamentoAtivoController extends Controller
+{
+    //
+}
