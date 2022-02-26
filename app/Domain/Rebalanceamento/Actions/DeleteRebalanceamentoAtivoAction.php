8fd3892650b2e3acf4e37c5e03d380835017d663
José Roberto <tekinforroberto@gmail.com>
2022-02-26 17:10:34 -0300

    feature: finalizado crud rebalanceamento por Ativo

---
+++ app/Domain/Rebalanceamento/Actions/DeleteRebalanceamentoAtivoAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\Rebalanceamento\Actions;
+
+use App\Models\RebalanceamentoAtivo;
+
+class DeleteRebalanceamentoAtivoAction
+{
+    public function __invoke($id)
+    {
+        $rebalanceamentoAtivo = RebalanceamentoAtivo::find($id);
+
+        if (is_null($rebalanceamentoAtivo)) {
+            throw new \Exception('Rebalanceamento por ativo não encontrada.');
+        }
+
+        $rebalanceamentoAtivo->delete();
+    }
+}
