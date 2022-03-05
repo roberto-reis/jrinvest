4ddce998ce5dc8109f2cc1343a015dc871682fbf
José Roberto <tekinforroberto@gmail.com>
2022-03-04 22:31:24 -0300

    refactor: Refatoração do Job CotacaoJob

--- app/Domain/Dashboard/Controllers/DashboardController.php
+++ app/Domain/Dashboard/Controllers/DashboardController.php
@@ -4,10 +4,8 @@ namespace App\Domain\Dashboard\Controllers;
 
 use Inertia\Inertia;
 use App\Models\Cotacao;
-use App\Models\Operacao;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Domain\Carteira\Jobs\MontaCarteiraJob;
 
 class DashboardController extends Controller
 {
