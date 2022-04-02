3963b63b2def7f5c58c922684db27711f52d003f
José Roberto <tekinforroberto@gmail.com>
2022-04-02 16:46:21 -0300

    Criado migrate rentabilidades_carteiras

---
+++ database/migrations/2022_04_02_194127_create_rentabilidades_carteiras_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentabilidadesCarteirasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentabilidades_carteiras', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->index();
+            $table->string('custo_total_carteira')->index();
+            $table->string('valor_total_carteira')->index();
+            $table->string('rentabilidade_valor');
+            $table->string('rentabilidade_percentual');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rentabilidades_carteiras');
+    }
+}
